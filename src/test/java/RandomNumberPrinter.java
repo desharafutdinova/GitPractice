69bc6139ab7201901d3ded6338f89f379ed45917
dianasharafutdinova <d.e.sharafutdinova@gmail.com>
2023-05-16 21:17:05 -0400

    Added overloaded method & new Test class

--- src/test/java/RandomNumberPrinter.java
+++ src/test/java/RandomNumberPrinter.java
@@ -10,4 +10,12 @@ public class RandomNumberPrinter {
         }
     }
 
+    public static void printRandomNumbers(int limit) {
+        Random random = new Random();
+        for (int i = 0; i < limit; i++) {
+            int randomNumber = random.nextInt();
+            System.out.println(randomNumber);
+        }
+    }
+
 }
