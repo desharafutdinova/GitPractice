802cb0a37f4f74063bcae789a19c26c330347e97
Nazakat2335 <naza.beybutova.89@gmail.com>
2023-06-06 17:14:51 -0500

    my first message

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -4,6 +4,9 @@ public class Test1 {
     public static void main(String[] args) {
 
         RandomNumberPrinter.printRandomNumbersLimit100();
+        String str="Hello world";
+        String str1="Hello world";
+        System.out.println(str1.equals(str));
 
     }
 }
